31a9408754371073b18eabcb54b598ed87a863d4
hipfel <silvan.steiger@gmx.net>
2022-04-26 17:13:58 +0200

    Summer_Loads Cosntruct
    Konstrukt für Summer Loads fertiggestellt. Basis für verbesserungen.

---
+++ .ipynb_checkpoints/sumloadcalc-checkpoint.py
@@ -0,0 +1,13 @@
+import numpy as np
+import psychro as psy
+
+
+# A * x = b ==> x = inv(A) * b
+# constants
+c = 1e3                     # air specific heat J/kg K
+l = 2496e3                  # latent heat J/kg
+
+
+def inputsummerload(m=1, θ0=3, φ0=0.8, θ1=32, φ1=0.95, α=0.5)
+
+
