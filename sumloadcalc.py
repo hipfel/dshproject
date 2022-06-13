f55979f770c73cc2594ab7496007764556fb0255
immanuelplattner <immanuel.plattner@outlook.com>
2022-06-13 12:10:59 +0200

    Main.ipynb: 	- Changed the default conditions to 35°C and 60% outdoors, and 25°C and 50% indoors for all the whole notebook. 	- Removed the enthalpy computations from the section "Air conditioning with cooling and dehumidification". 	- Added the number of air changes per hour (ACH) to the air conditioning widget. 	- Added the "correct" heat computation absorbtion process using the "savings factor".
    sumloadcalc.py:
    	- Changed the default conditions to 35°C and 60% outdoors, and 25°C and 50% indoors.

--- sumloadcalc.py
+++ sumloadcalc.py
@@ -11,7 +11,7 @@ c = 1e3                     # air specific heat J/kg K
 l = 2496e3                  # latent heat J/kg
 
 # Function for just creating widget to define parameters and later on saving them in variables
-def summerloadcalc(θ0=30, φ0=0.8, θi=23, φi=0.6):
+def summerloadcalc(θ0=35, φ0=0.6, θi=25, φi=0.5):
 
     print(f'θ0 = {θ0:5.1f} °C, φ0 = {φ0*100:5.0f} %')
     print(f'θi = {θi:5.1f} °C, φi = {φi*100:5.0f} %')
